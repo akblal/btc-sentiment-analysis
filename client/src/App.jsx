3a84906ef3f5525ad56a312471b2202121126cac
Brandon Hsu <brandon.j.hsu@gmail.com>
2022-10-14 23:00:01 -0700

    added mui components (dropdown menu, textfield, button)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-regular-svg-icons'
+import { FormControl, InputLabel, Select, MenuItem, Box, TextField, Button } from '@mui/material';
 
 import CoinPrice from './components/CoinPrice.jsx';
 import Sentiment from './components/Sentiment.jsx';
@@ -14,6 +15,7 @@ function App (props) {
 
   const [theme, setTheme] = useState('light');
   const [crypto, setCrypto] = useState('');
+  const [age, setAge] = useState(0);
 
   const handleSubmit = (e) => {
     const selected = e.target.value;
@@ -34,7 +36,7 @@ function App (props) {
 
   return (
     <div className = {`App ${theme}`}>
-      <div>
+      <div className= 'toggle-theme-container'>
         {theme === 'light' ?
           <FontAwesomeIcon icon={faMoon} onClick= {toggleTheme} className= 'toggle-theme-button' /> :
           <FontAwesomeIcon icon={faSun} onClick= {toggleTheme} className= 'toggle-theme-button' />
@@ -53,6 +55,34 @@ function App (props) {
           <option value= 'polygon'>Polygon</option>;
           <option value= 'cardano'>Cardano</option>;
         </select>
+      </div>
+
+      <div className= 'mui-container' >
+        <FormControl className= 'mui-component'>
+          <InputLabel id="demo-simple-select-label">Crypto</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={crypto}
+            label="Age"
+            onChange={handleSubmit}
+          >
+            <MenuItem value={'bitcoin'}>Bitcoin</MenuItem>
+            <MenuItem value={'ethereum'}>Ethereum</MenuItem>
+            <MenuItem value={'cardano'}>Cardano</MenuItem>
+            <MenuItem value={'polygon'}>Polygon</MenuItem>
+          </Select>
+        </FormControl>
+        <div className= 'search-crypto-field-and-button'>
+          <TextField
+          required
+          id="filled-required"
+          label="Search for Crypto"
+          defaultValue="Coin"
+          className= 'mui-component'
+          />
+          <Button variant="outlined" className= 'mui-submit-button'>Submit</Button>
+        </div>
       </div>
 
       <div className= 'price-and-sentiment'>
